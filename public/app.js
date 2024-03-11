81a14e56b5eee205fc747edbac0040ce7ed879bb
AminMarayed <aminmarayed4@gmail.com>
2024-03-11 11:27:53 +0330

    feat: handle add todo function with submit btn

--- public/app.js
+++ public/app.js
@@ -3,12 +3,17 @@ const showModalBtn = document.querySelector("#show-modal");
 const closeModalBtn = document.querySelector("#close-modal");
 const modal = document.querySelector("#modal");
 const backdrop = document.querySelector("#backdrop");
-
+const submitBtn = document.querySelector("#submit");
+const todoInput = document.querySelector("#input");
 // event
 showModalBtn.addEventListener("click", showModal);
 [closeModalBtn, backdrop].forEach((element) =>
   element.addEventListener("click", closeModal)
 );
+submitBtn.addEventListener("click", addTodo);
+
+// default varaible
+let todos = [];
 // function
 function showModal() {
   // show modal and backdrop
@@ -18,4 +23,21 @@ function showModal() {
 function closeModal() {
   // close modal and backdrop
   [modal, backdrop].forEach((element) => (element.style.display = "none"));
+}
+
+function addTodo(e) {
+  // don't refresh
+  e.preventDefault();
+
+  // add todo object in todos
+  todos.push({
+    id: Date.now(),
+    taskName: todoInput.value,
+    priority: "low",
+    status: "todo",
+  });
+
+  // reset todo input value
+  todoInput.value = "";
+  console.log(todos);
 }
