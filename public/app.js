f42ba7ba9e24fcf90018ad285ac0e088b1c44e69
AminMarayed <aminmarayed4@gmail.com>
2024-03-11 11:08:45 +0330

    feat: handle close modal and backdrop

--- public/app.js
+++ public/app.js
@@ -1,13 +1,21 @@
 // selector
 const showModalBtn = document.querySelector("#show-modal");
+const closeModalBtn = document.querySelector("#close-modal");
 const modal = document.querySelector("#modal");
 const backdrop = document.querySelector("#backdrop");
 
 // event
 showModalBtn.addEventListener("click", showModal);
-
+[closeModalBtn, backdrop].forEach((element) =>
+  element.addEventListener("click", closeModal)
+);
 // function
 function showModal() {
   // show modal and backdrop
   [modal, backdrop].forEach((element) => (element.style.display = "flex"));
+}
+
+function closeModal() {
+  // close modal and backdrop
+  [modal, backdrop].forEach((element) => (element.style.display = "none"));
 }
